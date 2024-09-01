ba82ccec4b008e16c0f520d78837f21c3e573230
YibeiLi777i <lyb7770128@gmail.com>
2024-09-02 02:13:47 +1000

    modify alert component and test pull request

--- apps/www/registry/sydney/example/alert-demo.tsx
+++ apps/www/registry/sydney/example/alert-demo.tsx
@@ -1,3 +1,4 @@
+import React from "react"
 import { Terminal } from "lucide-react"
 
 import {
@@ -5,15 +6,33 @@ import {
   AlertDescription,
   AlertTitle,
 } from "@/registry/default/ui/alert"
+import { Toaster } from "@/registry/default/ui/toaster"
+import { useToast } from "@/registry/default/ui/use-toast"
 
 export default function AlertDemo() {
+  const { toast } = useToast()
+
+  function handleCheckClick() {
+    toast({
+      title: "Information Check",
+      description: `You have check the message!`,
+    })
+  }
+
   return (
-    <Alert>
-      <Terminal className="h-4 w-4" />
-      <AlertTitle>Heads up!</AlertTitle>
-      <AlertDescription>
-        You can add components to your app using the cli.
-      </AlertDescription>
-    </Alert>
+    <>
+  <Toaster />
+  <Alert className="relative group overflow-hidden bg-white text-black transition-colors duration-500 ease-in-out hover:bg-orange-500 hover:text-white">
+    <Terminal className="h-4 w-4" />
+    <AlertTitle>Information Update!</AlertTitle>
+    Here is new information for you!{" "}
+    <span
+      onClick={() => handleCheckClick()}
+      className="transition-colors duration-500 ease-in-out bg-orange-500 text-white inline-flex items-center justify-center px-5 py-1 rounded-2xl absolute right-4 group-hover:bg-white group-hover:text-orange-500"
+    >
+      Check!
+    </span>
+  </Alert>
+</>
   )
 }
