a23107f4f7b86f8b31ea20805f5c4300832530d2
ZhengjiaPu <helenap7231@gmail.com>
2024-09-15 16:17:17 +1000

    feat(component): add variants of input

---
+++ apps/www/registry/sydney/example/input-with-phonenumber.tsx
@@ -0,0 +1,59 @@
+import * as React from "react";
+import { Button } from "@/registry/default/ui/button";
+import {
+    DropdownMenu,
+    DropdownMenuCheckboxItem,
+    DropdownMenuContent,
+    DropdownMenuTrigger,
+} from "@/registry/default/ui/dropdown-menu";
+import { Input } from "@/registry/default/ui/input"
+
+export function InputPhoneNumber() {
+    const [selectedCountry, setSelectedCountry] = React.useState("+61");
+
+    const handleCountrySelect = (country) => {
+        setSelectedCountry(country);
+    };
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant="outline">{selectedCountry}</Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56">
+                <DropdownMenuCheckboxItem
+                    checked={selectedCountry === "US"}
+                    onCheckedChange={() => handleCountrySelect("+1")}
+                >
+                    United States (+1)
+                </DropdownMenuCheckboxItem>
+                <DropdownMenuCheckboxItem
+                    checked={selectedCountry === "UK"}
+                    onCheckedChange={() => handleCountrySelect("+44")}
+                >
+                    United Kingdom (+44)
+                </DropdownMenuCheckboxItem>
+
+                <DropdownMenuCheckboxItem
+                    checked={selectedCountry === "AUS"}
+                    onCheckedChange={() => handleCountrySelect("+61")}
+                >
+                    Australia (+61)
+                </DropdownMenuCheckboxItem>
+                <DropdownMenuCheckboxItem
+                    checked={selectedCountry === "CHN"}
+                    onCheckedChange={() => handleCountrySelect("+86")}
+                >
+                    China (+86)
+                </DropdownMenuCheckboxItem>
+            </DropdownMenuContent>
+
+            <Input type="number" placeholder="123-456-7890" style={{ width: '50%' }} />
+
+        </DropdownMenu>
+
+    )
+
+}
+
+export default InputPhoneNumber;
+
