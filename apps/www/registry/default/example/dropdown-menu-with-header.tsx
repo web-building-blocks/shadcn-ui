cff2e2abb328537394a44ea6e7f5b810ad1459fd
wbszzy1231 <linkerdzc@gmail.com>
2024-09-20 19:03:36 +1000

    variation

---
+++ apps/www/registry/default/example/dropdown-menu-with-header.tsx
@@ -0,0 +1,71 @@
+"use client"
+
+import * as React from "react"
+
+import { Button } from "@/registry/sydney/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/registry/sydney/ui/dropdown-menu"
+
+export function DropdownMenuwithHeader() {
+  const userEmail = "nzha9876@uni.sydney.edu.au"
+
+  const [selectedOptions, setSelectedOptions] = React.useState([])
+
+  const toggleOption = (option) => {
+    setSelectedOptions((prev) =>
+      prev.includes(option)
+        ? prev.filter((item) => item !== option)
+        : [...prev, option]
+    )
+  }
+
+  const getButtonLabel = () => {
+    if (selectedOptions.length === 0) return "Select Options"
+    return `Selected: ${selectedOptions.join(", ")}`
+  }
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline">{getButtonLabel()}</Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuLabel>Signed in as</DropdownMenuLabel>
+        <div className="email">{userEmail}</div>
+        <DropdownMenuSeparator />
+        <DropdownMenuCheckboxItem
+          checked={selectedOptions.includes("Account Settings")}
+          onCheckedChange={() => toggleOption("Account Settings")}
+        >
+          Account Settings
+        </DropdownMenuCheckboxItem>
+        <DropdownMenuCheckboxItem
+          checked={selectedOptions.includes("Support")}
+          onCheckedChange={() => toggleOption("Support")}
+        >
+          Support
+        </DropdownMenuCheckboxItem>
+        <DropdownMenuCheckboxItem
+          checked={selectedOptions.includes("License")}
+          onCheckedChange={() => toggleOption("License")}
+        >
+          License
+        </DropdownMenuCheckboxItem>
+        <DropdownMenuCheckboxItem
+          checked={selectedOptions.includes("Signout")}
+          onCheckedChange={() => toggleOption("Signout")}
+        >
+          Signout
+        </DropdownMenuCheckboxItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
+
+export default DropdownMenuwithHeader
