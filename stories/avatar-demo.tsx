73885db313f36cde319b1b2e74ae8a6e8d3e2742
ZhengjiaPu <helenap7231@gmail.com>
2024-09-29 19:40:46 +1000

    feat(avatar): update avatar demo and add stories

--- stories/avatar-demo.tsx
+++ stories/avatar-demo.tsx
@@ -31,67 +31,54 @@ import {
   DropdownMenuTrigger,
 } from "@/registry/default/ui/dropdown-menu"
 
-export function AvatarDemo() {
+function AvatarDemo({ username, email, avatarSrc, size }) {
   const openGitHub = () => {
-    window.open(
-      "https://github.com/Programming-is-like-breathing-air",
-      "_blank"
-    )
-  }
+    window.open("https://github.com/Programming-is-like-breathing-air", "_blank");
+  };
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <div style={{ cursor: "pointer" }}>
-          <Avatar style={{ width: "50px", height: "50px" }}>
-            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+          <Avatar style={{ width: `${size}px`, height: `${size}px` }}>
+            <AvatarImage
+              src={avatarSrc}
+              alt={username}
+              style={{
+                width: '100%',
+                height: '100%',
+                objectFit: 'cover'  // 确保图片按比例填满容器
+              }}
+            />
             <AvatarFallback>CN</AvatarFallback>
           </Avatar>
         </div>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-72">
         <DropdownMenuLabel>
-          Mariana Castilho
-          <br /> notmyreal@email.com{" "}
+          {username}
+          <br /> {email}
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuItem>
-          <User className="mr-2 h-4 w-4" />
           <span>Profile</span>
-          <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
         </DropdownMenuItem>
         <DropdownMenuItem>
-          <CreditCard className="mr-2 h-4 w-4" />
           <span>Billing</span>
-          <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
         </DropdownMenuItem>
         <DropdownMenuItem>
-          <Settings className="mr-2 h-4 w-4" />
           <span>Settings</span>
-          <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
-        </DropdownMenuItem>
-        <DropdownMenuItem>
-          <Users className="mr-2 h-4 w-4" />
-          <span>Team</span>
-        </DropdownMenuItem>
-        <DropdownMenuItem>
-          <Plus className="mr-2 h-4 w-4" />
-          <span>New Team</span>
-          <DropdownMenuShortcut>⌘+T</DropdownMenuShortcut>
         </DropdownMenuItem>
         <DropdownMenuItem onClick={openGitHub}>
-          <Github className="mr-2 h-4 w-4" />
           <span>GitHub</span>
         </DropdownMenuItem>
         <DropdownMenuSeparator />
         <DropdownMenuItem style={{ color: "#EF4444" }}>
-          <LogOut className="mr-2 h-4 w-4" />
           <span>Log out</span>
-          <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
-  )
+  );
 }
 
-export default AvatarDemo
+export default AvatarDemo;
