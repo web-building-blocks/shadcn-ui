e02d3674169dbe52b871e9607ef56afe88f3cb8e
Cody Hsiang <codyhsiang995@gmail.com>
2024-11-04 20:11:14 +1100

    feat(component): comment component, styles(sydney & IBM), doc

--- apps/www/registry/ibm/example/comment-demo.tsx
+++ apps/www/registry/ibm/example/comment-demo.tsx
@@ -1,171 +1,13 @@
-// import React, { useState } from "react"
-// import { Forward, Heart, MessageCircleMore } from "lucide-react"
+import React, { useState } from "react"
+import { Forward, Heart, MessageCircleMore } from "lucide-react"
 
-// import { Avatar, AvatarImage } from "@/registry/ibm/ui/avatar"
-// import { Button } from "@/registry/ibm/ui/button"
-// import { Card, CardContent, CardHeader } from "@/registry/ibm/ui/card"
-// import { Label } from "@/registry/ibm/ui/label"
-// import { Textarea } from "@/registry/ibm/ui/textarea"
-// import { Toaster } from "@/registry/ibm/ui/toaster"
-// import { useToast } from "@/registry/ibm/ui/use-toast"
-
-// export function BasicComment() {
-//   const initialMessage = {
-//     user: "USYD Student",
-//     avatarUrl: "https://api.dicebear.com/7.x/miniavs/svg?seed=1",
-//     description:
-//       "I am a student of USYD and majoring in Master of Computer Science.",
-//     content:
-//       "Education for all, leadership for good. Progressive thinking, breaking with convention, challenging the status quo and improving the world around us is in our DNA.",
-//     likes: 156,
-//     comments: 156,
-//     shares: 2,
-//   }
-
-//   const [message, setMessage] = useState(initialMessage)
-//   const [isTextareaActive, setIsTextareaActive] = useState(false)
-//   const [commentText, setCommentText] = useState("")
-//   const { toast } = useToast()
-
-//   const incrementStat = (stat: "likes" | "comments" | "shares") => {
-//     if (stat === "comments") {
-//       setIsTextareaActive(!isTextareaActive) // Toggle textarea visibility
-//       setCommentText("") // Reset the comment text
-//     } else {
-//       setMessage((prev) => ({ ...prev, [stat]: prev[stat] + 1 }))
-//     }
-//   }
-
-//   const postComment = () => {
-//     if (commentText.trim()) {
-//       toast({
-//         title: "You have successfully posted your comment:",
-//         description: commentText,
-//       })
-
-//       setMessage((prev) => ({ ...prev, comments: prev.comments + 1 }))
-//       setIsTextareaActive(false)
-//       setCommentText("")
-//     }
-//   }
-
-//   return (
-//     <>
-//       <Toaster />
-//       <Card className="mb-4">
-//         <CardHeader
-//           className="flex flex-col p-4"
-//           style={{ borderBottom: "1px solid #e8e8e8" }}
-//         >
-//           <div className="flex items-center mb-2">
-//             <Avatar style={{ marginRight: "10px" }}>
-//               <AvatarImage src={message.avatarUrl} alt={message.user} />
-//             </Avatar>
-//             <div className="flex flex-col">
-//               <Label style={{ fontWeight: "bold", fontSize: "1.05rem" }}>
-//                 {message.user}
-//               </Label>
-//             </div>
-//           </div>
-//           <div className="flex flex-col" style={{ marginLeft: "50px" }}>
-//             <Label style={{ fontSize: "0.75rem", color: "gray" }}>
-//               {message.description}
-//             </Label>
-//           </div>
-//         </CardHeader>
-//         <CardContent>
-//           <div
-//             className="text-left"
-//             style={{
-//               fontSize: "0.95rem",
-//               marginTop: "20px",
-//               marginBottom: "10px",
-//             }}
-//           >
-//             {message.content}
-//           </div>
-//           <div
-//             className="flex mt-2 justify-left text-sm text-gray-400 space-x-1"
-//             style={{ marginTop: "20px" }}
-//           >
-//             <Button
-//               variant="link"
-//               onClick={() => incrementStat("likes")}
-//               style={{ color: "gray" }}
-//             >
-//               <Heart size={20} style={{ marginRight: "5px" }} />
-//               {message.likes}
-//             </Button>
-//             <span
-//               className="text-gray-100 mx-1"
-//               style={{ marginTop: "10px", color: "gray" }}
-//             >
-//               |
-//             </span>
-//             <Button
-//               variant="link"
-//               onClick={() => incrementStat("comments")}
-//               style={{ color: "gray" }}
-//             >
-//               <MessageCircleMore size={20} style={{ marginRight: "5px" }} />
-//               {message.comments}
-//             </Button>
-//             <span
-//               className="text-gray-400 mx-1"
-//               style={{ marginTop: "10px", color: "gray" }}
-//             >
-//               |
-//             </span>
-//             <Button
-//               variant="link"
-//               onClick={() => incrementStat("shares")}
-//               style={{ color: "gray" }}
-//             >
-//               <Forward size={20} style={{ marginRight: "5px" }} />
-//               {message.shares}
-//             </Button>
-//           </div>
-//           {isTextareaActive && (
-//             <div className="grid w-full gap-2 mt-2">
-//               <Textarea
-//                 value={commentText}
-//                 placeholder="Type your comment here."
-//                 onChange={(e) => setCommentText(e.target.value)}
-//               />
-//               <div className="flex justify-end space-x-2">
-//                 <Button
-//                   onClick={() => setIsTextareaActive(false)}
-//                   variant="secondary"
-//                 >
-//                   Close
-//                 </Button>
-//                 <Button
-//                   onClick={postComment}
-//                   variant="default"
-//                   disabled={!commentText.trim()}
-//                 >
-//                   Post Comment
-//                 </Button>
-//               </div>
-//             </div>
-//           )}
-//         </CardContent>
-//       </Card>
-//     </>
-//   )
-// }
-
-// export default BasicComment
-import React, { useState } from "react";
-import { Forward, Heart, MessageCircleMore } from "lucide-react";
-
-import { Avatar, AvatarImage } from "@/registry/ibm/ui/avatar";
-import { Button } from "@/registry/ibm/ui/button";
-import { Card, CardContent, CardHeader } from "@/registry/ibm/ui/card";
-import { Label } from "@/registry/ibm/ui/label";
-import { Textarea } from "@/registry/ibm/ui/textarea";
-import { Toaster } from "@/registry/ibm/ui/toaster";
-import { useToast } from "@/registry/ibm/ui/use-toast";
+import { Avatar, AvatarImage } from "@/registry/ibm/ui/avatar"
+import { Button } from "@/registry/ibm/ui/button"
+import { Card, CardContent, CardHeader } from "@/registry/ibm/ui/card"
+import { Label } from "@/registry/ibm/ui/label"
+import { Textarea } from "@/registry/ibm/ui/textarea"
+import { Toaster } from "@/registry/ibm/ui/toaster"
+import { useToast } from "@/registry/ibm/ui/use-toast"
 
 export function BasicComment() {
   const initialMessage = {
@@ -178,34 +20,34 @@ export function BasicComment() {
     likes: 156,
     comments: 156,
     shares: 2,
-  };
+  }
 
-  const [message, setMessage] = useState(initialMessage);
-  const [isTextareaActive, setIsTextareaActive] = useState(false);
-  const [commentText, setCommentText] = useState("");
-  const { toast } = useToast();
+  const [message, setMessage] = useState(initialMessage)
+  const [isTextareaActive, setIsTextareaActive] = useState(false)
+  const [commentText, setCommentText] = useState("")
+  const { toast } = useToast()
 
   const incrementStat = (stat: "likes" | "comments" | "shares") => {
     if (stat === "comments") {
-      setIsTextareaActive(!isTextareaActive);
-      setCommentText("");
+      setIsTextareaActive(!isTextareaActive)
+      setCommentText("")
     } else {
-      setMessage((prev) => ({ ...prev, [stat]: prev[stat] + 1 }));
+      setMessage((prev) => ({ ...prev, [stat]: prev[stat] + 1 }))
     }
-  };
+  }
 
   const postComment = () => {
     if (commentText.trim()) {
       toast({
         title: "You have successfully posted your comment:",
         description: commentText,
-      });
+      })
 
-      setMessage((prev) => ({ ...prev, comments: prev.comments + 1 }));
-      setIsTextareaActive(false);
-      setCommentText("");
+      setMessage((prev) => ({ ...prev, comments: prev.comments + 1 }))
+      setIsTextareaActive(false)
+      setCommentText("")
     }
-  };
+  }
 
   return (
     <>
@@ -213,8 +55,8 @@ export function BasicComment() {
       <Card
         className="mb-4"
         style={{
-          backgroundColor: "#f4f4f4",  // Light gray background
-          borderRadius: "0px",        // Square corners
+          backgroundColor: "#f4f4f4", // Light gray background
+          borderRadius: "0px", // Square corners
           border: "1px solid #d1d1d1", // Light border
         }}
       >
@@ -222,7 +64,7 @@ export function BasicComment() {
           className="flex flex-col p-4"
           style={{
             backgroundColor: "#161616", // Black header
-            color: "#ffffff",           // White text for header
+            color: "#ffffff", // White text for header
             borderBottom: "1px solid #d1d1d1",
             fontFamily: "IBM Plex Sans, sans-serif",
           }}
@@ -276,7 +118,11 @@ export function BasicComment() {
             <Button
               variant="link"
               onClick={() => incrementStat("likes")}
-              style={{ color: "#6f6f6f", display: "flex", alignItems: "center" }}
+              style={{
+                color: "#6f6f6f",
+                display: "flex",
+                alignItems: "center",
+              }}
             >
               <Heart size={20} style={{ marginRight: "5px" }} />
               {message.likes}
@@ -290,7 +136,11 @@ export function BasicComment() {
             <Button
               variant="link"
               onClick={() => incrementStat("comments")}
-              style={{ color: "#6f6f6f", display: "flex", alignItems: "center" }}
+              style={{
+                color: "#6f6f6f",
+                display: "flex",
+                alignItems: "center",
+              }}
             >
               <MessageCircleMore size={20} style={{ marginRight: "5px" }} />
               {message.comments}
@@ -304,7 +154,11 @@ export function BasicComment() {
             <Button
               variant="link"
               onClick={() => incrementStat("shares")}
-              style={{ color: "#6f6f6f", display: "flex", alignItems: "center" }}
+              style={{
+                color: "#6f6f6f",
+                display: "flex",
+                alignItems: "center",
+              }}
             >
               <Forward size={20} style={{ marginRight: "5px" }} />
               {message.shares}
@@ -355,7 +209,7 @@ export function BasicComment() {
         </CardContent>
       </Card>
     </>
-  );
+  )
 }
 
-export default BasicComment;
+export default BasicComment
--- apps/www/registry/sydney/example/comment-demo.tsx
+++ apps/www/registry/sydney/example/comment-demo.tsx
@@ -1,161 +1,3 @@
-// import React, { useState } from "react"
-// import { Forward, Heart, MessageCircleMore } from "lucide-react"
-
-// import { Avatar, AvatarImage } from "@/registry/sydney/ui/avatar"
-// import { Button } from "@/registry/sydney/ui/button"
-// import { Card, CardContent, CardHeader } from "@/registry/sydney/ui/card"
-// import { Label } from "@/registry/sydney/ui/label"
-// import { Textarea } from "@/registry/sydney/ui/textarea"
-// import { Toaster } from "@/registry/sydney/ui/toaster"
-// import { useToast } from "@/registry/sydney/ui/use-toast"
-
-// export function BasicComment() {
-//   const initialMessage = {
-//     user: "USYD Student",
-//     avatarUrl: "https://api.dicebear.com/7.x/miniavs/svg?seed=1",
-//     description:
-//       "I am a student of USYD and majoring in Master of Computer Science.",
-//     content:
-//       "Education for all, leadership for good. Progressive thinking, breaking with convention, challenging the status quo and improving the world around us is in our DNA.",
-//     likes: 156,
-//     comments: 156,
-//     shares: 2,
-//   }
-
-//   const [message, setMessage] = useState(initialMessage)
-//   const [isTextareaActive, setIsTextareaActive] = useState(false)
-//   const [commentText, setCommentText] = useState("")
-//   const { toast } = useToast()
-
-//   const incrementStat = (stat: "likes" | "comments" | "shares") => {
-//     if (stat === "comments") {
-//       setIsTextareaActive(!isTextareaActive) // Toggle textarea visibility
-//       setCommentText("") // Reset the comment text
-//     } else {
-//       setMessage((prev) => ({ ...prev, [stat]: prev[stat] + 1 }))
-//     }
-//   }
-
-//   const postComment = () => {
-//     if (commentText.trim()) {
-//       toast({
-//         title: "You have successfully posted your comment:",
-//         description: commentText,
-//       })
-
-//       setMessage((prev) => ({ ...prev, comments: prev.comments + 1 }))
-//       setIsTextareaActive(false)
-//       setCommentText("")
-//     }
-//   }
-
-//   return (
-//     <>
-//       <Toaster />
-//       <Card className="mb-4">
-//         <CardHeader
-//           className="flex flex-col p-4"
-//           style={{ borderBottom: "1px solid #e8e8e8" }}
-//         >
-//           <div className="flex items-center mb-2">
-//             <Avatar style={{ marginRight: "10px" }}>
-//               <AvatarImage src={message.avatarUrl} alt={message.user} />
-//             </Avatar>
-//             <div className="flex flex-col">
-//               <Label style={{ fontWeight: "bold", fontSize: "1.05rem" }}>
-//                 {message.user}
-//               </Label>
-//             </div>
-//           </div>
-//           <div className="flex flex-col" style={{ marginLeft: "50px" }}>
-//             <Label style={{ fontSize: "0.75rem", color: "gray" }}>
-//               {message.description}
-//             </Label>
-//           </div>
-//         </CardHeader>
-//         <CardContent>
-//           <div
-//             className="text-left"
-//             style={{
-//               fontSize: "0.95rem",
-//               marginTop: "20px",
-//               marginBottom: "10px",
-//             }}
-//           >
-//             {message.content}
-//           </div>
-//           <div
-//             className="flex mt-2 justify-left text-sm text-gray-400 space-x-1"
-//             style={{ marginTop: "20px" }}
-//           >
-//             <Button
-//               variant="link"
-//               onClick={() => incrementStat("likes")}
-//               style={{ color: "gray" }}
-//             >
-//               <Heart size={20} style={{ marginRight: "5px" }} />
-//               {message.likes}
-//             </Button>
-//             <span
-//               className="text-gray-100 mx-1"
-//               style={{ marginTop: "10px", color: "gray" }}
-//             >
-//               |
-//             </span>
-//             <Button
-//               variant="link"
-//               onClick={() => incrementStat("comments")}
-//               style={{ color: "gray" }}
-//             >
-//               <MessageCircleMore size={20} style={{ marginRight: "5px" }} />
-//               {message.comments}
-//             </Button>
-//             <span
-//               className="text-gray-400 mx-1"
-//               style={{ marginTop: "10px", color: "gray" }}
-//             >
-//               |
-//             </span>
-//             <Button
-//               variant="link"
-//               onClick={() => incrementStat("shares")}
-//               style={{ color: "gray" }}
-//             >
-//               <Forward size={20} style={{ marginRight: "5px" }} />
-//               {message.shares}
-//             </Button>
-//           </div>
-//           {isTextareaActive && (
-//             <div className="grid w-full gap-2 mt-2">
-//               <Textarea
-//                 value={commentText}
-//                 placeholder="Type your comment here."
-//                 onChange={(e) => setCommentText(e.target.value)}
-//               />
-//               <div className="flex justify-end space-x-2">
-//                 <Button
-//                   onClick={() => setIsTextareaActive(false)}
-//                   variant="secondary"
-//                 >
-//                   Close
-//                 </Button>
-//                 <Button
-//                   onClick={postComment}
-//                   variant="default"
-//                   disabled={!commentText.trim()}
-//                 >
-//                   Post Comment
-//                 </Button>
-//               </div>
-//             </div>
-//           )}
-//         </CardContent>
-//       </Card>
-//     </>
-//   )
-// }
-
-// export default BasicComment
 import React, { useState } from "react"
 import { Forward, Heart, MessageCircleMore } from "lucide-react"
 
@@ -187,8 +29,8 @@ export function BasicComment() {
 
   const incrementStat = (stat: "likes" | "comments" | "shares") => {
     if (stat === "comments") {
-      setIsTextareaActive(!isTextareaActive) 
-      setCommentText("") 
+      setIsTextareaActive(!isTextareaActive)
+      setCommentText("")
     } else {
       setMessage((prev) => ({ ...prev, [stat]: prev[stat] + 1 }))
     }
@@ -211,19 +53,35 @@ export function BasicComment() {
     <>
       <Toaster />
       <Card className="mb-4" style={{ borderColor: "#EAEAEA" }}>
-        <CardHeader className="flex flex-col p-4" style={{ borderBottom: "1px solid #EAEAEA" }}>
+        <CardHeader
+          className="flex flex-col p-4"
+          style={{ borderBottom: "1px solid #EAEAEA" }}
+        >
           <div className="flex items-center mb-2">
             <Avatar style={{ marginRight: "10px" }}>
               <AvatarImage src={message.avatarUrl} alt={message.user} />
             </Avatar>
             <div className="flex flex-col">
-              <Label style={{ fontFamily: "Apercu Pro", fontWeight: "bold", fontSize: "1.05rem", color: "#333333" }}>
+              <Label
+                style={{
+                  fontFamily: "Apercu Pro",
+                  fontWeight: "bold",
+                  fontSize: "1.05rem",
+                  color: "#333333",
+                }}
+              >
                 {message.user}
               </Label>
             </div>
           </div>
           <div className="flex flex-col" style={{ marginLeft: "50px" }}>
-            <Label style={{ fontFamily: "Apercu Pro", fontSize: "0.85rem", color: "gray" }}>
+            <Label
+              style={{
+                fontFamily: "Apercu Pro",
+                fontSize: "0.85rem",
+                color: "gray",
+              }}
+            >
               {message.description}
             </Label>
           </div>
@@ -241,18 +99,37 @@ export function BasicComment() {
           >
             {message.content}
           </div>
-          <div className="flex mt-2 justify-left text-sm space-x-2" style={{ color: "#666666", marginTop: "20px" }}>
-            <Button variant="link" onClick={() => incrementStat("likes")} style={{ color: "#666666" }}>
+          <div
+            className="flex mt-2 justify-left text-sm space-x-2"
+            style={{ color: "#666666", marginTop: "20px" }}
+          >
+            <Button
+              variant="link"
+              onClick={() => incrementStat("likes")}
+              style={{ color: "#666666" }}
+            >
               <Heart size={20} style={{ marginRight: "5px" }} />
               {message.likes}
             </Button>
-            <span className="text-gray-300 mx-1" style={{ color: "#EAEAEA" }}>|</span>
-            <Button variant="link" onClick={() => incrementStat("comments")} style={{ color: "#666666" }}>
+            <span className="text-gray-300 mx-1" style={{ color: "#EAEAEA" }}>
+              |
+            </span>
+            <Button
+              variant="link"
+              onClick={() => incrementStat("comments")}
+              style={{ color: "#666666" }}
+            >
               <MessageCircleMore size={20} style={{ marginRight: "5px" }} />
               {message.comments}
             </Button>
-            <span className="text-gray-300 mx-1" style={{ color: "#EAEAEA" }}>|</span>
-            <Button variant="link" onClick={() => incrementStat("shares")} style={{ color: "#666666" }}>
+            <span className="text-gray-300 mx-1" style={{ color: "#EAEAEA" }}>
+              |
+            </span>
+            <Button
+              variant="link"
+              onClick={() => incrementStat("shares")}
+              style={{ color: "#666666" }}
+            >
               <Forward size={20} style={{ marginRight: "5px" }} />
               {message.shares}
             </Button>
@@ -266,10 +143,23 @@ export function BasicComment() {
                 style={{ fontFamily: "Apercu Pro", color: "#333333" }}
               />
               <div className="flex justify-end space-x-2">
-                <Button onClick={() => setIsTextareaActive(false)} variant="secondary" style={{ fontFamily: "Apercu Pro" }}>
+                <Button
+                  onClick={() => setIsTextareaActive(false)}
+                  variant="secondary"
+                  style={{ fontFamily: "Apercu Pro" }}
+                >
                   Close
                 </Button>
-                <Button onClick={postComment} variant="default" disabled={!commentText.trim()} style={{ fontFamily: "Apercu Pro", backgroundColor: "#FF6F00", color: "#FFFFFF" }}>
+                <Button
+                  onClick={postComment}
+                  variant="default"
+                  disabled={!commentText.trim()}
+                  style={{
+                    fontFamily: "Apercu Pro",
+                    backgroundColor: "#FF6F00",
+                    color: "#FFFFFF",
+                  }}
+                >
                   Post Comment
                 </Button>
               </div>
