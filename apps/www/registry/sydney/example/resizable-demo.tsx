6258ae53044436e3b44479686e414d5bcfd966eb
Cody Hsiang <codyhsiang995@gmail.com>
2024-11-16 20:38:08 +1100

    feat(component): update resizable component, styles, storybook, doc

--- apps/www/registry/sydney/example/resizable-demo.tsx
+++ apps/www/registry/sydney/example/resizable-demo.tsx
@@ -2,31 +2,31 @@ import {
   ResizableHandle,
   ResizablePanel,
   ResizablePanelGroup,
-} from "@/registry/default/ui/resizable"
+} from "@/registry/sydney/ui/resizable"
 
 export default function ResizableDemo() {
   return (
     <ResizablePanelGroup
       direction="horizontal"
-      className="max-w-md rounded-lg border"
+      className="max-w-md rounded-lg border border-charcoal bg-white"
     >
       <ResizablePanel defaultSize={50}>
-        <div className="flex h-[200px] items-center justify-center p-6">
-          <span className="font-semibold">One</span>
+        <div className="flex h-[200px] items-center justify-center p-6 bg-ochre text-charcoal">
+          <span className="font-sans font-semibold">One</span>
         </div>
       </ResizablePanel>
-      <ResizableHandle />
+      <ResizableHandle className="bg-charcoal border-l border-ochre" />
       <ResizablePanel defaultSize={50}>
         <ResizablePanelGroup direction="vertical">
           <ResizablePanel defaultSize={25}>
-            <div className="flex h-full items-center justify-center p-6">
-              <span className="font-semibold">Two</span>
+            <div className="flex h-full items-center justify-center p-6 bg-charcoal text-ochre">
+              <span className="font-sans font-semibold">Two</span>
             </div>
           </ResizablePanel>
-          <ResizableHandle />
+          <ResizableHandle className="bg-charcoal border-t border-ochre" />
           <ResizablePanel defaultSize={75}>
-            <div className="flex h-full items-center justify-center p-6">
-              <span className="font-semibold">Three</span>
+            <div className="flex h-full items-center justify-center p-6 bg-ochre text-charcoal">
+              <span className="font-sans font-semibold">Three</span>
             </div>
           </ResizablePanel>
         </ResizablePanelGroup>
--- apps/www/registry/sydney/example/resizable-vertical.tsx
+++ apps/www/registry/sydney/example/resizable-vertical.tsx
@@ -2,23 +2,23 @@ import {
   ResizableHandle,
   ResizablePanel,
   ResizablePanelGroup,
-} from "@/registry/default/ui/resizable"
+} from "@/registry/sydney/ui/resizable"
 
 export default function ResizableDemo() {
   return (
     <ResizablePanelGroup
       direction="vertical"
-      className="min-h-[200px] max-w-md rounded-lg border"
+      className="min-h-[200px] max-w-md rounded-lg border border-charcoal bg-white"
     >
       <ResizablePanel defaultSize={25}>
-        <div className="flex h-full items-center justify-center p-6">
-          <span className="font-semibold">Header</span>
+        <div className="flex h-full items-center justify-center p-6 bg-ochre text-charcoal">
+          <span className="font-sans font-semibold">Header</span>
         </div>
       </ResizablePanel>
-      <ResizableHandle />
+      <ResizableHandle className="bg-charcoal border-t border-ochre" />
       <ResizablePanel defaultSize={75}>
-        <div className="flex h-full items-center justify-center p-6">
-          <span className="font-semibold">Content</span>
+        <div className="flex h-full items-center justify-center p-6 bg-charcoal text-ochre">
+          <span className="font-sans font-semibold">Content</span>
         </div>
       </ResizablePanel>
     </ResizablePanelGroup>
