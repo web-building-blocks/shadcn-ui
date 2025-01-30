7d7c275ea7a400de73f90a9203971f1c83e56dee
HoshinoIzumi <hoshinoitsumi@gmail.com>
2025-01-30 22:17:21 +1100

    fix: registry schema UNSW

--- apps/www/registry/schema.ts
+++ apps/www/registry/schema.ts
@@ -41,7 +41,7 @@ export type Registry = z.infer<typeof registrySchema>
 
 export const blockSchema = registryEntrySchema.extend({
   type: z.literal("components:block"),
-  style: z.enum(["default", "new-york", "sydney", "ibm"]),
+  style: z.enum(["default", "new-york", "sydney", "ibm", "UNSW"]),
   component: z.any(),
   container: z
     .object({
